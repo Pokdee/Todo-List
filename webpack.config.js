67e0fc0c29400c3aa4a9ec501c1bed341bdffede
Pokdee <loselife61@gmail.com>
2023-06-30 00:41:30 +0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/js/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|git)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 9000,
+    open: true,
+  },
+};
