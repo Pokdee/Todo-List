ab65cb675c86e5fc33346d507860784dbac64acc
Pokdee <loselife61@gmail.com>
2023-07-21 23:44:18 +0700

    Implement and style new task adding system

--- src/script.js
+++ src/script.js
@@ -11,11 +11,13 @@ const tskForm = document.querySelector(".form_tsk");
 const tskInput = document.querySelector(".addtask_input");
 const addTask = document.querySelector(".add_tsk");
 const cancelTask = document.querySelector(".cancel_tsk");
+const tskCon = document.querySelector(".projects_ul");
 
 //change content
 navCon.addEventListener("click", (e) => {
+  ///navigate accross navigation
   const text = e.target.textContent;
-  contents.innerHTML = "";
+  // contents.innerHTML = "";
   const headHtml = `<h2 class="notes_heading">
   ${text}
 </h2>`;
@@ -25,24 +27,38 @@ Add Task
 </button>`;
   contents.insertAdjacentHTML("afterbegin", headHtml);
 
-  /////
+  /////act according to project section
   if (e.target.classList.contains("pj")) {
     contents.insertAdjacentHTML("beforeend", btnHtml);
+
     //add new task
     const newTaskBtn = document.querySelector(".btn_addtsk");
 
     newTaskBtn.addEventListener("click", () => {
       newTaskBtn.classList.add("hide");
       tskForm.classList.remove("hide");
-      ////
+
+      ////manage new task form
       tskForm.addEventListener("submit", (e) => {
         e.preventDefault();
         const tskName = tskInput.value;
         if (tskName) {
           console.log(tskName);
-          tskInput.value = "";
           newTaskBtn.classList.remove("hide");
           tskForm.classList.add("hide");
+
+          ///Html
+          const tskHtml = `<li class="projects_li">
+          <input class="check_in" type="checkbox" name="" id="note" />
+          <label class="check_lb" for="note">${tskName}</label>
+          <span class="date">7/21/23</span>
+          <div class="delete_btn">
+            <i class="fas fa-multiply"></i>
+          </div>
+        </li>`;
+          // console.log(tskHtml);
+          console.log(tskCon);
+          tskCon.insertAdjacentHTML("afterbegin", tskHtml);
         }
       });
     });
